bbe4c5ba09266a56a48fa9c3dd9bb43f0ee3ff4b
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-09-15 16:55:26 -0300

    eliminado

--- Back/src/advertising spot/advertisingSpot.entity.ts
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto'
-
-export class AdvertisingSpot {
-  constructor(
-    public name: string,
-    public duration: number,
-    public id = crypto.randomUUID()
-  ) {}
-}
