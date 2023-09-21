3ad99e202cc59144b827c9254c20f51ff4612fbf
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-09-20 22:31:45 -0300

    agregado routing

--- Front/Radio/src/app/app-routing.module.ts
+++ Front/Radio/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
+import { DummyComponent } from './dummy/dummy.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login', component: DummyComponent },
+  { path:'altaComercio', component: DummyComponent },
+  { path:'actualizacionComercio', component: DummyComponent },
+  { path:'altaPublicista', component: DummyComponent },
+  { path:'liquidacionPublicistas', component: DummyComponent },
+  { path:'listadoLiquidaciones', component: DummyComponent },
+  { path:'registroPagoPub', component: DummyComponent },
+  { path:'actualizacionPublicista', component: DummyComponent },
+  { path:'altaContratacion', component: DummyComponent },
+  { path:'actualizacionContratacion', component: DummyComponent },
+  { path:'registroPago', component: DummyComponent },
+  { path:'informeFaltaPago', component: DummyComponent },
+  { path:'emisionOrdenes', component: DummyComponent },
+  { path:'listadoPublicitario', component: DummyComponent },
+  { path:'actualizacionSpot', component: DummyComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/Radio/src/app/app.module.ts
+++ Front/Radio/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DummyComponent } from './dummy/dummy.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    MainNavComponent
+    MainNavComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
