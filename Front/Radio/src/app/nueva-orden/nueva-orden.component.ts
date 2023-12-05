690bd40d6615320b326fa344f282e25f1c51cc09
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-12-05 16:52:25 -0300

    Nueva orden en ingles
    #8 Closed

--- Front/Radio/src/app/nueva-orden/nueva-orden.component.ts
+++ Front/Radio/src/app/nueva-orden/nueva-orden.component.ts
@@ -1,7 +1,7 @@
-import { Component ,ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
 import { MyDataService } from '../services/my-data.service';
 import { OnInit } from '@angular/core';
-import { take ,tap} from 'rxjs/operators';
+import { take, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { ThemePalette } from '@angular/material/core/index.js';
 import { OrdenFechaComponent } from '../orden-fecha/orden-fecha.component';
@@ -11,103 +11,50 @@ import { OrdenFechaComponent } from '../orden-fecha/orden-fecha.component';
   templateUrl: './nueva-orden.component.html',
   styleUrls: ['./nueva-orden.component.scss']
 })
-export class NuevaOrdenComponent implements OnInit{
+export class NuevaOrdenComponent implements OnInit {
   @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
-  numero:any =null
-  inValido: boolean= false
-  coloring: ThemePalette = "primary"
-  dataSource : any|null = null
-  fecha:any|null
-  igual:any= true
-  
+  number: any = null;
+  isInvalid: boolean = false;
+  colorPalette: ThemePalette = "primary";
+  data: any | null = null;
+  date: any | null = null;
+  same: any = true;
 
-  displayedColumns: string[] = ['Bloque', 'Lunes', 'Martes', 'Miercoles','Jueves','Viernes','Sabado','Domingo'];
-  constructor(private myDataService: MyDataService,private router: Router) { }
+  displayedColumns: string[] = ['Bloque', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+  constructor(private myDataService: MyDataService, private router: Router) { }
 
   redirectToHome() {
     this.router.navigate(['/']);
   }
+
   ngOnInit() {
-    this.getBLoques();
+    this.getBlocks();
   }
-  getBLoques() {
+
+  getBlocks() {
     this.myDataService.getBlocks().pipe(take(1)).subscribe(data => {
-      let blocks :any
+      let blocks: any;
       blocks = data;
       blocks = blocks.data;
-      this.dataSource = blocks;
-    } );
+      this.data = blocks;
+    });
   }
+
   onCheckboxChange(index: number, day: string) {
-  // Aquí puedes registrar las celdas tildadas en un array o realizar otras acciones.
-  if (this.dataSource[index][day]) {
-    // La celda está tildada, realiza la lógica necesaria.
-  } else {
-    // La celda está destildada, realiza la lógica necesaria.
+    // Aquí puedes registrar las celdas tildadas en un array o realizar otras acciones.
+    if (this.data[index][day]) {
+      // La celda está tildada, realiza la lógica necesaria.
+    } else {
+      // La celda está destildada, realiza la lógica necesaria.
+    }
   }
-}
-crearFecha(){
-  this.container.createComponent(OrdenFechaComponent)
-}
-  // verify() {
-  //   console.log(this.selectedValue);
-  //   console.log(this.nuevoPrecio);
-  //   if (!this.isCreateHistoyDisabled() ) {
-  //     this.inValido = false;
-  //     this.createHistory();
-     
-  //   } else {
-  //     this.inValido = true;
-  //   this.coloring = "warn";
-  //   if(!this.selectedValue){
-  //    this.inputBloque.focus(); 
-      
-  //   }else{
-  //     const precioInputElement = this.precioInputRef.nativeElement;
-  //     precioInputElement.focus();
-  //   }
-    
-  //   }
-  // }  
-isCreateContracDisabled() {
-  return !this.numero || isNaN(this.numero);
-}  
-// createContrac() {
-//     // Obtén la fecha de hoy en el formato "yyyy-MM-dd"
-//     const today = new Date(); // Esto ya incluirá la hora actual, minutos y segundos
-//     const year = today.getFullYear();
-//     const month = String(today.getMonth() + 1).padStart(2, '0');
-//     const day = String(today.getDate()).padStart(2, '0');
-//     const hours = String(today.getHours()).padStart(2, '0');
-//     const minutes = String(today.getMinutes()).padStart(2, '0');
-//     const seconds = String(today.getSeconds()).padStart(2, '0');
-//     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-//     // Obten el valor del precio desde el input (debes almacenar esto en una propiedad del componente)
-//     const precio = this.nuevoPrecio;
-//     console.log(precio);
 
-//     // Obtén el id del bloque seleccionado (asumiendo que este valor se almacena en selectedValue.id)
-//     const idBlock = this.selectedValue.id;
+  createDate() {
+    this.container.createComponent(OrdenFechaComponent);
+  }
 
-//     // Crea el objeto newHistory con los valores requeridos
-//     const newHistory = {
-//       startTime: formattedDate,
-//       precio: precio,
-//       idBlock: idBlock
-//     };
-
-//     // Ahora puedes usar newHistory como necesites, por ejemplo, enviarlo a tu servicio para crear un historial
-//     this.myDataService.createHistory(newHistory).subscribe(
-//       (response) => {
-//         console.log('Historial creado:', response);
-//         // Puedes realizar acciones adicionales después de crear un historial
-//         this.getHistory();
-//       },
-//       (error) => {
-//         console.error('Error al crear historial:', error);
-//         // Maneja los errores aquí
-//       }
-//     );
-//   }
-
+  isCreateContractDisabled() {
+    return !this.number || isNaN(this.number);
+  }
 }
