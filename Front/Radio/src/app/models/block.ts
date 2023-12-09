e4398bc332d874a855b61603e7da4d315f8e875b
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2023-12-09 00:34:58 -0300

    primer intento de interfaces en bloques
    #2

--- Front/Radio/src/app/models/block.ts
+++ Front/Radio/src/app/models/block.ts
@@ -1,9 +1,5 @@
-export class Block {
-    id?: string;
-    numbre: string;
-    startTime: string;
-    constructor( numbre: string, startTime: string) {
-        this.numbre = numbre;
-        this.startTime = startTime;
-    }
+export interface Block {
+  id?: string;
+  number: string;
+  startTime: string;
 }
