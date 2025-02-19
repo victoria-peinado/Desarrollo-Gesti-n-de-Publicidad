aa8adcb6e75da3685a4bc2c169cc3f66d0437459
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2025-02-19 15:26:48 -0300

    inicio test en front #38

---
+++ Front/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = { 
+displayName: 'Radio', 
+maxWorkers: 3, 
+bail: false,//stop when fail 
+preset: 'jest-preset-angular', 
+roots: ['<rootDir>/src/',], 
+testMatch: ['**/+(*.)+(spec).+(ts)'], 
+setupFilesAfterEnv: ['<rootDir>/src/test.ts'], 
+collectCoverage: true, 
+cacheDirectory: '<rootDir>/jestCache', 
+coverageReporters: ['text-summary', 'lcov'], 
+coverageDirectory: 'coverage/Radio', 
+globals: { 
+  'ts-jest': { 
+    tsconfig: '<rootDir>/tsconfig.spec.json', 
+    }, 
+  }, 
+}; 
