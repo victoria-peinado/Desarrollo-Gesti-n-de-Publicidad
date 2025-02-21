df6fd2bd07aa9c4acfc5fa0606b7c8e435bf95af
Marcos <m4rc0s.d3l.s0l4r@gmail.com>
2025-02-20 21:51:53 -0300

    1st section edicion-comercio added

--- Front/src/app/pages/comercios-page/categories/edicion-comercio-category/edicion-comercio-category.component.ts
+++ Front/src/app/pages/comercios-page/categories/edicion-comercio-category/edicion-comercio-category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edicion-comercio-category',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './edicion-comercio-category.component.scss'
 })
 export class EdicionComercioCategoryComponent {
+  titular_form: FormGroup;
 
+  constructor() {
+    this.titular_form = new FormGroup({
+      cuit: new FormControl('', [Validators.required, Validators.maxLength(11),Validators.minLength(11),]),
+      comercio: new FormControl('', Validators.required)
+    });
+  }
+
+  siguiente() {
+    console.log(this.titular_form.value);
+  }
+
+  get cuitControl(): FormControl {
+    return this.titular_form.get('cuit') as FormControl;
+  }
+  
+  get comercioControl(): FormControl {
+    return this.titular_form.get('comercio') as FormControl;
+  }
 }
