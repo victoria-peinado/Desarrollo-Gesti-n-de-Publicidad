807f404078d5c08d6267523129c220c5fe902b6b
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2025-02-24 18:00:28 -0300

    cambios back

---
+++ Back/tests/feo.spec.ts
@@ -0,0 +1,21 @@
+
+
+// describe('Basic Math Operations', () => {
+//   it('should add 3 + 4 to equal 7', () => {
+//     expect(3 + 4).toBe(7);
+//   });
+
+//   it('should subtract 4 - 3 to equal 1', () => {
+//     expect(4 - 3).toBe(1);
+//   });
+
+//   it('should multiply 3 * 4 to equal 12', () => {
+//     expect(3 * 4).toBe(12);
+//   });
+
+//   it('should divide 12 / 4 to equal 3', () => {
+//     expect(12 / 4).toBe(3);
+//   });
+// });
+
+// // We recommend installing an extension to run jest tests.
