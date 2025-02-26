0490384d9c37afa72e9b6d302866c7535b4a9dbe
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2025-02-25 21:10:18 -0300

    cambiado el archioivo de inicio

--- Back/tests/app.spec.ts
+++ Back/tests/app.spec.ts
@@ -1,16 +1,18 @@
 
+ import { app } from '../src/app.js';
+ import request from 'supertest';
 describe('POST /api/auth', () => {
   test('should return 200 and a token if the credentials are correct', async () => {
     console.log('test');
-    // const response = await request(app)
-    //   .post('/api/auth/login')
-    //   .send({ 
-    //     username: 'admin',
-    //     password: 'admin'
-    //   });
-    // expect(response.status).toBe(200);
-    // expect(response.body).toHaveProperty('username');
-    // expect(response.body).toHaveProperty('password');
-    // expect(response.body).toHaveProperty('token');
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({ 
+        username: 'admin',
+        password: 'admin'
+      });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('username');
+    expect(response.body).toHaveProperty('password');
+    expect(response.body).toHaveProperty('token');
   });
 } );
