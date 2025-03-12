1d67aa3bfa2681543273b140ad166664c617534c
Lautaro Brancatti <126621984+Lautaro498@users.noreply.github.com>
2025-03-12 15:10:41 -0300

    MERGE block-selection.component.ts

--- Front/src/app/components/block-selection/block-selection.component.ts
+++ Front/src/app/components/block-selection/block-selection.component.ts
@@ -1,6 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, input, effect, Input,  SimpleChange, output, signal } from '@angular/core';
 import { BLOCK_TIMES } from 'src/app/constants/constants';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { addDays, eachDayOfInterval, endOfMonth, format, parse } from 'date-fns';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-block-selection',
@@ -19,67 +21,115 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ],
 })
 export class BlockSelectionComponent {
+
+
+
+
+
+
   daysOfWeek: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-  isNoRegular: boolean = false;
+  regularCampo = false
   customDates: string[] = [];
+
+
+
   blocksPerDay: { [key: string]: { id: number; time: string }[] } = {};
+
   private idCounter = 0;
 
   blockTimes = BLOCK_TIMES;
 
+
+  month = input.required<string>()
+
+  
+
+  datesOfMonth: Date[] = [];
+  datesOfMonthString: string[] =[]
+  
+
+  formatFunction = (date: Date) => { return format(date, 'dd') }
+
+  @Output() emmiterNoRegular = new EventEmitter<boolean>();
+
   @Output() regStructureChange = new EventEmitter<{ [key: string]: string[] }>();
 
+  @Output() notRegularStructure = new EventEmitter<[string, string[]][]>(); //date en format yyyy-mm-dd y arreglo blockNum
+
   constructor() {
-    this.initializeDays();
+    this.initializeDays(this.daysOfWeek);
+
+
+    effect(() => {
+      this.diasDelMesSeleccionado()
+    })
+    
+    //effect(() => { this.changeEmitter() })
+    
+
   }
+
+  sanitizarYemitir() {
+    if (!this.regularCampo) {
+      this.emitRegStructure()
+    } else { this.emitNotRegStructure() }
+  }
+
+  changeEmitter(){
+    //console.log('Estoy emitiendo: ', this.emmiterNoRegular )
+    this.emmiterNoRegular
+    .pipe(tap((valor) => console.log('Valor emitido:', valor)))
+    .subscribe();
+    this.emmiterNoRegular.emit(this.regularCampo)
+  }
+
 
   toggleMode() {
-    if (this.isNoRegular) {
-      this.blocksPerDay = {};
-      this.customDates = [];
+    this.changeEmitter()
+    if (this.regularCampo) {
+      //this.diasDelMesSeleccionado()
+      //funcion para limpiar
+      this.initializeDays(this.datesOfMonthString);
+      
     } else {
-      this.initializeDays();
+      this.initializeDays(this.daysOfWeek);
     }
-
-    this.emitRegStructure();
   }
 
-  private initializeDays() {
+  private initializeDays(keysArray: string[]) {
     this.blocksPerDay = {};
-    this.daysOfWeek.forEach(day => this.blocksPerDay[day] = []);
+    keysArray.forEach(day => this.blocksPerDay[day] = []);
   }
 
-  addCustomDate(event: any) { //se comenta y se borra
-    const date: Date = event.value;
-    if (date) {
-      const formattedDate = this.formatDate(date);
-      if (!this.customDates.includes(formattedDate)) {
-        this.customDates.push(formattedDate);
-        this.blocksPerDay[formattedDate] = [];
-        this.emitRegStructure();
-      }
-    }
-  }
+  // addCustomDate(event: any) {
+  //   const date: Date = event.value;
+  //   if (date) {
+  //     const formattedDate = this.formatDate(date);
+  //     if (!this.customDates.includes(formattedDate)) {
+  //       this.customDates.push(formattedDate);
+  //       this.blocksPerDay[formattedDate] = [];
+  //     }
+  //   }
+  // }
 
   addBlock(label: string) {
     this.blocksPerDay[label].push({ id: this.idCounter++, time: '' });
-    this.emitRegStructure(); //no va
+    
   }
 
   removeBlock(label: string, id: number) {
     this.blocksPerDay[label] = this.blocksPerDay[label].filter(b => b.id !== id);
-    this.emitRegStructure(); //no va
+    
   }
 
   updateBlock(label: string, id: number, value: string) {
     const block = this.blocksPerDay[label].find(b => b.id === id);
-    if (block){ block.time = value;
-    this.emitRegStructure(); } //no va
+    if (block) block.time = value;
   }
 
   generateRegStructure(): { [key: string]: string[] } {
     const result: { [key: string]: string[] } = {};
-    const keys = this.isNoRegular ? this.customDates : this.daysOfWeek;
+    const keys = this.regularCampo ? this.customDates : this.daysOfWeek;
 
     for (const key of keys) {
       result[this.formatKey(key)] = this.blocksPerDay[key]
@@ -90,33 +140,72 @@ export class BlockSelectionComponent {
     return result;
   }
 
+  // blocksPerDay: { [key: string]: { id: number; time: string }[] } = {}; 
+  //                   stringDate:     usoInterno    paraMapearConBlockNum
+  generateNotRegularStructure(){
+    const notRegularStructure: [string, string[]][] = []
+    const data = this.blocksPerDay
+    const dateStrings = Object.keys(data)
+    for (const dayString of dateStrings){
+      if (data[dayString].length>0){
+      const day = new Date(dayString)
+      const parseDay = format(day, 'yyyy-MM-dd') //asi se va al back
+      const blocksNumsOfDay = data[dayString].map((obj: { id: number, time: string }) => { return this.timeToBlockId(obj.time)}) //arreglo de bloques num que van en el dia
+      notRegularStructure.push([parseDay, blocksNumsOfDay]) } 
+      else {delete data[dayString]}
+    }
+    console.log(notRegularStructure)
+    return notRegularStructure
+  }
+
   private formatKey(key: string): string {
     const map: { [key: string]: string } = {
       'Lunes': 'monday', 'Martes': 'tuesday', 'Miércoles': 'wednesday',
       'Jueves': 'thursday', 'Viernes': 'friday', 'Sábado': 'saturday', 'Domingo': 'sunday',
     };
-    return this.isNoRegular ? key : map[key];
+    return this.regularCampo ? key : map[key];
   }
 
+    //devuelve el numBlock dada un timeStart
   private timeToBlockId(time: string): string {
     const [hour, minute] = time.split(':').map(Number);
     return ((hour * 2) + (minute === 30 ? 1 : 0)).toString();
   }
 
   private formatDate(date: Date): string {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Meses en JS son 0-indexed
-    const year = date.getFullYear();
-    return `${day}-${month}-${year}`;
+    return format(date, 'yyyy-MM-dd')
   }
-  
+
+  emitNotRegStructure(){
+    const notRegularStructure = this.generateNotRegularStructure()
+    this.notRegularStructure.emit(notRegularStructure)
+
+  }
 
   emitRegStructure() {
     const regStructure = this.generateRegStructure();
-    console.log('regStructure', regStructure);
     this.regStructureChange.emit(regStructure);
   }
 
-  
-  
+  diasDelMesSeleccionado() {
+    let month = this.month()
+    const today = new Date()
+    const actualMonth = format(today, 'MM-yyyy')
+    if (!this.month()) { month = actualMonth }
+    if (actualMonth == month) {
+      //si es del mismo mes listo desde mañana hasta el final del mes. 
+      const lastDay = endOfMonth(today)
+      const tomorrow = addDays(today, 1)
+      this.datesOfMonth = eachDayOfInterval({ start: tomorrow, end: lastDay })
+    } else {
+      const firstDay = parse((month + '-01'), 'MM-yyyy-dd', new Date())
+      const lastDay = endOfMonth(firstDay)
+      this.datesOfMonth = eachDayOfInterval({ start: firstDay, end: lastDay })
+    }
+    this.datesOfMonth.forEach((day) => { this.datesOfMonthString.push(day.toString())})
+    console.log(this.datesOfMonth)
+  }
+
+
 }
+
